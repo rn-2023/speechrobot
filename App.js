56972129dc20c34800ad7d9e7387e70881eed284
pekkao <pekka.ojala@oamk.fi>
2023-09-13 18:19:17 +0300

    Basic version

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import * as Speech from 'expo-speech';
+
+const aphorism = [
+  'Never', 'trust', 'a', 'computer', 'you', 'can', 
+  'not', 'throw', 'out', 'a', 'window'
+];
+const NBR_OF_REPEATS = 5;
 
 export default function App() {
+  const speak = () => {
+    for (let i = 0; i < NBR_OF_REPEATS; i++) {
+      let shuffledAphorism = shuffle(aphorism);
+      let thingToSay = '';
+      shuffledAphorism.map(word => {
+        thingToSay += word;
+      })
+      Speech.speak(thingToSay);
+    }
+  };  
+
+  const shuffle = (array) => {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button style={styles.button} title="Gimme some random nonsense..." onPress={speak} />
     </View>
   );
 }
